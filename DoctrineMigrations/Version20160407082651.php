efa42d03455a08840ff447f0a55c019b9ac0fff5
Julien Maulny <doubjulien@hotmail.fr>
2016-04-07 11:13:23 +0200

    add date_first_query column

---
+++ DoctrineMigrations/Version20160407082651.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160407082651 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE stat_compiled.users ADD COLUMN date_first_request time without time zone;');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE stat_compiled.users DROP COLUMN date_first_request;');
+    }
+}
